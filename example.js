d0f4fa05a47a54ebbedc89e8652788fd67cd549b
Brent-T <brenttimmermans7@gmail.com>
2020-12-14 23:14:15 +0100

    update example 📚

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
 const fixPhotos = require('./index')
 const path = require('path')
 
-const DIR_PATH = path.resolve('/Users/brent/Downloads/542_1910181358')
-const DATE = '01/10/2019'
+const DIR_PATH = path.resolve('/Users/user/Pictures/some-folder-with-scans')
+const DATE = '01/01/2020'
 const ROLL_NUMBER = 26
 
 fixPhotos(DIR_PATH, ROLL_NUMBER, DATE)
